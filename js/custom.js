4560b5379c559e84eb4d3a089b3ecfce052a9e91
dqbubme <david@tgandtechnologies.com>
2024-09-20 15:30:11 -0700

    upgrade tgand tech site to a food marketing service website
    looks good to go except the links + I want to add a blog

--- js/custom.js
+++ js/custom.js
@@ -20,7 +20,7 @@
 
     // BACKSTRETCH SLIDESHOW
     $('#section_1').backstretch([
-      "images/slide/surface-1x5jnhtlp3Y-unsplash.jpg",
+      "images/slide/foodslide.png",
     ],  {duration: 2000, fade: 750});
     
   })(window.jQuery);
