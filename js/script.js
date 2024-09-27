8b1d37738d3737f61e242b635d4e333b004e3e8e
dqbubme <david@tgandtechnologies.com>
2024-09-27 09:56:11 -0700

    added aleert
    added alert

--- js/script.js
+++ js/script.js
@@ -5,5 +5,6 @@ function sendMail(){
         subject : document.getElementById('subject').value,       
         message : document.getElementById('message').value       
     }
-    emailjs.send('service_0j3997j', 'template_dtq1u2b', params).then.alert('Email Sent'); 
+    emailjs.send('service_0j3997j', 'template_dtq1u2b', params);
+    alert('Email Sent'); 
 }
