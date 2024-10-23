c5fc8974bab53bf2c2167705cb9cc49fc18e36c6
dqbubme <david@tgandtechnologies.com>
2024-10-23 08:44:03 -0700

    updated the email section
    Email section added more info EmailsJS

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ function sendMail(){
     let params = {
         name : document.getElementById('name').value,
         email : document.getElementById('email').value,
+        phone : document.getElementById('phone').value,
+        time : document.getElementById('time').value,
         subject : document.getElementById('subject').value,       
         message : document.getElementById('message').value       
     }
